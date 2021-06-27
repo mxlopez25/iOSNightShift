bcc3a0accd21bb8438007925c67893720253b9e2
Maximo Lopez <maximo.lopez95@gmail.com>
2021-06-27 04:19:45 -0400

    First UI stage finished

--- NightWatch/ContentView.swift
+++ NightWatch/ContentView.swift
@@ -12,33 +12,66 @@ struct ContentView: View {
         HStack {
             VStack {
                 VStack(alignment: .leading) {
-                    Text("Nightly Tasks")
-                        .underline()
-                        .font(.title2)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.blue)
-                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                    Text("Check all windows")
-                    Text("Check all doors")
-                    Text("Check that the safe is locked")
-                    Text("Check the mailbox")
-                    Text("Check the mailbox")
-                    Text("Check the security camera")
-                    Text("Document \"Strange and unusual\" occurrences ")
-                    Text("Weekly Tasks")
-                        .underline()
-                        .font(.title2)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.blue)
-                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                    Text("Monthly Tasks")
-                        .underline()
-                        .font(.title2)
-                        .fontWeight(.heavy)
-                        .foregroundColor(.blue)
-                        .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
-                    
+                    // MARK: Nightly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "moon.stars"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                                .padding(.bottom)
+                             Text("Nightly Tasks")
+                                .underline()
+                                .font(.title2)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .padding(.bottom)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                        Text("Check all windows")
+                        Text("Check all doors")
+                        Text("Check that the safe is locked")
+                        Text("Check the mailbox")
+                        Text("Check the mailbox")
+                        Text("Check the security camera")
+                        Text("Document \"Strange and unusual\" occurrences ")
+                    }
+                    // MARK: Weekly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "sunset"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Weekly Tasks")
+                                .underline()
+                                .font(.title2)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .padding(.top)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                    }
+                    // MARK: Monthly Tasks
+                    Group {
+                        Divider()
+                        HStack {
+                            Text(Image(systemName: "calendar"))
+                                .foregroundColor(.yellow)
+                                .font(.title3)
+                                .fontWeight(.heavy)
+                            Text("Monthly Tasks")
+                                .underline()
+                                .font(.title2)
+                                .fontWeight(.heavy)
+                                .foregroundColor(.yellow)
+                                .textCase(/*@START_MENU_TOKEN@*/.uppercase/*@END_MENU_TOKEN@*/)
+                        }
+                    }
                 }
+                .padding([.top, .leading, .bottom])
                 Spacer()
             }
             Spacer()
