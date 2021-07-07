9ea124e403ee41a0584cd026613e411253836e75
Maximo Lopez <maximo.lopez@endurance.com>
2021-07-06 21:50:51 -0400

    Task model added, Update on xcodeproject file

---
+++ NightWatch/Models/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  NightWatch
+//
+//  Created by Maximo Lopez on 7/6/21.
+//
+
+import Foundation
+
+struct Task {
+    let name: String
+    var isComplete: Bool
+    var lastCompleted: Date?
+}
